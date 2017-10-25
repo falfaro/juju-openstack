8752bd8a133170f60a01300219b51c1f3097a988
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2017-10-25 23:12:25 +0200

    Small improvements to deploy-4-nodes.sh.

--- deploy-4-nodes.sh
+++ deploy-4-nodes.sh
@@ -4,11 +4,11 @@ set -e
 : ${JUJU_BIN:=/snap/bin/juju}
 
 $JUJU_BIN version
-$JUJU_BIN kill-controller openstack-base-hw-x -y || true
+$JUJU_BIN destroy-controller openstack-base-hw-x -y || true
 $JUJU_BIN bootstrap maas-hw openstack-base-hw-x --config=./maas-hw-config.yaml --to node-31
 $JUJU_BIN switch openstack-base-hw-x:controller
 
-sleep 5
+sleep 15
 
 $JUJU_BIN deploy -m openstack-base-hw-x:controller bundle-4-nodes.yaml
 #$JUJU_BIN remove-unit -m openstack-base-hw-x:controller openstack-dashboard/0
