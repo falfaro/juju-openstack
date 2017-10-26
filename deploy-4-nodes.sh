4ccbfb225e2a69b981b666256ee68f3b85b35d32
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2017-10-26 02:15:42 +0200

    Remove spurious comments from deploy-4-nodes.sh

--- deploy-4-nodes.sh
+++ deploy-4-nodes.sh
@@ -11,14 +11,4 @@ $JUJU_BIN switch openstack-base-hw-x:controller
 sleep 15
 
 $JUJU_BIN deploy -m openstack-base-hw-x:controller bundle-4-nodes.yaml
-#$JUJU_BIN remove-unit -m openstack-base-hw-x:controller openstack-dashboard/0
-#$JUJU_BIN add-unit -m openstack-base-hw-x:controller openstack-dashboard --to lxd:0
-#$JUJU_BIN remove-unit -m openstack-base-hw-x:controller keystone/0
-#$JUJU_BIN add-unit -m openstack-base-hw-x:controller keystone --to lxd:0
-#$JUJU_BIN add-unit -m openstack-base-hw-x:controller ceph-osd --to 0
-#$JUJU_BIN add-unit -m openstack-base-hw-x:controller ceph-mon --to lxd:0
-#$JUJU_BIN add-unit -m openstack-base-hw-x:controller nova-compute --to 0
-
 watch -c "$JUJU_BIN status --color -m openstack-base-hw-x:controller"
-
-#$JUJU_BIN kill-controller openstack-base-hw-x
