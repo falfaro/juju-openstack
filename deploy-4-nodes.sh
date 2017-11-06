81d295444ed0a3068dede2261c97870a9e32fa40
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2017-11-06 15:47:12 +0100

    Bundles for Newton and Ocata.

--- deploy-4-nodes.sh
+++ deploy-4-nodes.sh
@@ -4,11 +4,11 @@ set -e
 : ${JUJU_BIN:=/snap/bin/juju}
 
 $JUJU_BIN version
-$JUJU_BIN destroy-controller openstack-base-hw-x -y || true
+$JUJU_BIN destroy-controller --destroy-all-models openstack-base-hw-x -y || true
 $JUJU_BIN bootstrap maas-hw openstack-base-hw-x --config=./maas-hw-config.yaml --to node-31
 $JUJU_BIN switch openstack-base-hw-x:controller
 
 sleep 15
 
-$JUJU_BIN deploy -m openstack-base-hw-x:controller bundle-4-nodes.yaml
+$JUJU_BIN deploy -m openstack-base-hw-x:controller bundle-4-nodes.newton.yaml
 watch -c "$JUJU_BIN status --color -m openstack-base-hw-x:controller"
