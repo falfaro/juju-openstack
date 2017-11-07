0689fa5382b00f957481cadbebe0283ef38fcda8
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2017-11-07 12:28:43 +0100

    Enable HA for Keystone. Rearrange some units for better machine placement.

--- deploy-4-nodes.sh
+++ deploy-4-nodes.sh
@@ -5,10 +5,12 @@ set -e
 
 $JUJU_BIN version
 $JUJU_BIN destroy-controller --destroy-all-models openstack-base-hw-x -y || true
-$JUJU_BIN bootstrap maas-hw openstack-base-hw-x --config=./maas-hw-config.yaml --to node-31
+$JUJU_BIN bootstrap maas-hw openstack-base-hw-x --config=./maas-hw-config.yaml --to node-32
 $JUJU_BIN switch openstack-base-hw-x:controller
 
 sleep 15
 
 $JUJU_BIN deploy -m openstack-base-hw-x:controller bundle-4-nodes.newton.yaml
-watch -c "$JUJU_BIN status --color -m openstack-base-hw-x:controller"
+#watch -c "$JUJU_BIN status --color -m openstack-base-hw-x:controller"
+git clone https://github.com/wwwtyro/juju-status.git
+watch -c ./juju-status/juju-status unit.workload unit.juju unit.message
