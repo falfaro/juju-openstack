a289a47b3d6a6ace5ff9ed77b563c3b7aa6f3963
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2018-01-17 19:46:56 +0100

    Merge post-install.sh into deploy.sh.

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,5 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 set -x
+
+function fail {
+  echo $@ >&2
+  exit 1
+}
+
+function retry {
+  local n=1
+  local max=10
+  local delay=1m
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max."
+        sleep $delay;
+      else
+        fail "The command '$@' has failed after $n attempts."
+      fi
+    }
+  done
+}
 
 juju destroy-model -y kubernetes
 juju add-model kubernetes
@@ -8,8 +30,12 @@ juju deploy bundle.flannel.yaml
 juju wait -w
 
 sudo snap install --classic kubectl 2>/dev/null
+sudo snap install helm 2>/dev/null
+
 mkdir -p ~/.kube
-juju scp kubernetes-master/0:config ~/.kube/config
+retry juju scp kubernetes-master/0:config ~/.kube/config
+mkdir -p ~/snap/helm/common/kube
+cp ~/.kube/config ~/snap/helm/common/kube/config
 
 cat rbd-storage-class.sh | juju ssh --pty=false kubernetes-master/0 -- bash -l
 
@@ -17,3 +43,6 @@ kubectl create -f coredns.yaml
 
 DNS=$(kubectl --namespace=kube-system get -o json service coredns | jq .spec.clusterIP)
 juju config kubernetes-worker kubelet-extra-args='cluster-dns='${DNS}' cluster-domain=cluster.local'
+
+kubectl create -f helm-rbac-config.yaml
+helm init --service-account tiller
