902054802423cfacf2238212a491b3367c2448f3
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2018-01-17 18:32:13 +0100

    Better post-installation.
    - Install Helm client
    - Retry SCP of kubectl config from the Kubernetes master: it is generated
      asynchronously after 'juju wait' considers deployment is done.

--- kubernetes/post-deploy.sh
+++ kubernetes/post-deploy.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+
+function fail {
+  echo $@ >&2
+  exit 1
+}
+
+function retry {
+  local n=1
+  local max=10
+  local delay=1m
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max."
+        sleep $delay;
+      else
+        fail "The command '$@' has failed after $n attempts."
+      fi
+    }
+  done
+}
+
 sudo snap install --classic kubectl
+sudo snap install helm
 mkdir -p ~/.kube
-juju scp kubernetes-master/0:config ~/.kube/config
+
+retry juju scp kubernetes-master/0:config ~/.kube/config
+
+mkdir -p ~/snap/helm/common/kube/config
+cp ~/.kube/config ~/snap/helm/common/kube/config
